004f2f92da46a2a52a7ae027b9bb797e7dfcff23
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 09:46:43 +0200

    installing react router and adding link to nav

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { NavLink as Link} from 'react-router-dom';
+
 
 export const Nav = styled.nav`
     background: transparent;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Nav } from './NavbarElements';
+import { Nav, NavLink } from './NavbarElements';
 
 const Navbar = () => {
     return (
         <>
-            <Nav />
+            <Nav>
+                <NavLink to="/">Pizza</NavLink>
+            </Nav>
         </>
     );
 }
