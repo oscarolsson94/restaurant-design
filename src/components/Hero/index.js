ab3ea978eb928a9dbd99efa26498b575a15068bf
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 10:46:31 +0200

    getting images and working on hero background

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Navbar from '../Navbar';
+import { HeroContainer } from './HeroElements';
 
 const Hero = () => {
     return (
-        <div>
-            
-        </div>
+        <HeroContainer>
+            <Navbar />
+        </HeroContainer>
     );
 }
 
 export default Hero;
+
+
