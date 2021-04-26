1b199494fbe0456ace574d3e21d017804602df00
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 09:43:14 +0200

    working on nav

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import styled from 'styled-component';
+import styled from 'styled-components';
 
-const NavbarElements = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+export const Nav = styled.nav`
+    background: transparent;
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    font-weight: 700;
+`;
 
-export default NavbarElements;
+export const NavLink = styled(Link)`
+    color: #fff;
+    font-size: 2rem;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    cursor: pointer;
+
+    @media screen and (max-width:400px) {
+        position: absolute;
+        top: 10px;
+        left: 25px;
+    }
+`;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Nav } from './NavbarElements';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Nav />
+        </>
     );
 }
 
