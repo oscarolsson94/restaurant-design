d3bbf628ada7d53ffef9d1f413d75a2705b96f02
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 10:08:30 +0200

    getting new fonts and starting hero component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
+import Hero from './components/Hero';
 
 
 
@@ -10,6 +11,7 @@ function App() {
     <Router>
         <GlobalStyle />
         <Navbar />
+        <Hero />
     </Router>
   );
 }
