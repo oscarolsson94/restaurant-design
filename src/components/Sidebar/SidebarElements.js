ac4a588ff965e90758084b18233725f0e6e10e24
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 12:17:24 +0200

    styling sidebarcontainer

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
 
 export const SidebarContainer = styled.div`
-
+    position: fixed;
+    z-index: 999;
+    width: 350px;
+    height: 100%;
+    background: #ffc500;
+    display: grid;
+    align-items: center;
+    top: 0;
+    transition: 0.3s ease-in-out;
+    right: ${({ isOpen }) => (isOpen ? '0' : '-1000px') };
 `;
 
 export const SidebarLink = styled.div`
