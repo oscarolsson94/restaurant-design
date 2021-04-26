723955e641088ec865dc4d0756dea7fd71c126c6
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 13:56:51 +0200

    starting products component

---
+++ src/components/Products/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <ProductsContainer>
+            <ProductsHeading></ProductsHeading>
+            <ProductsWrapper>
+                
+            </ProductsWrapper>
+            
+        </ProductsContainer>
+    );
+}
+
+export default Products;
