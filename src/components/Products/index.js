f7e763ff1cd85d81bc75c55f5572b44c3809af94
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 14:43:54 +0200

    products component layout and elements

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { ProductsContainer, ProductsWrapper, ProductsHeading, ProductTitle, ProductCard, ProductImg, ProductInfo, ProductDesc, ProductPrice, ProductButton } from './ProductsElements';
 
 const Products = () => {
     return (
         <ProductsContainer>
-            <ProductsHeading></ProductsHeading>
+            <ProductsHeading>heading</ProductsHeading>
             <ProductsWrapper>
-                
+                {data.map((product, index) => {
+                    return (
+                        <ProductCard key={index}>
+                            <ProductImg src={product.img} alt={product.alt} />
+                            <ProductInfo>
+                                <ProductTitle>{product.name}</ProductTitle>
+                                <ProductDesc>{product.desc}</ProductDesc>
+                                <ProductPrice>{product.price}</ProductPrice>
+                                <ProductButton>{product.button}</ProductButton>
+                            </ProductInfo>
+                        </ProductCard>
+                    )
+                })}
             </ProductsWrapper>
             
         </ProductsContainer>
