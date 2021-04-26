1d5bc862201f7159d934f54cb4f112986ffdd71e
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 10:54:15 +0200

    changing structure to make linear gradient easier

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -3,4 +3,7 @@ import ImgBg from '../../images/pizza-3.jpg';
 
 export const HeroContainer = styled.div`
     background: linear-gradient(to right, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.1)), url(${ImgBg});
+    height: 100vh;
+    background-position: center;
+    background-size: cover;
 `;
