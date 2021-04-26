10c0c6b3aa0165e5473c046c2e8e3125f7b3fec6
oscarolsson94 <69190482+oscarolsson94@users.noreply.github.com>
2021-04-26 12:32:57 +0200

    styling menu and links

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -35,8 +35,29 @@ export const Icon = styled.div`
 
 export const SidebarMenu = styled.div`
     display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat()(3, 80px);
+    text-align: center;
+
+    @media screen and (max-width: 400px){
+        grid-template-rows: repeat(3, 60px);
+    }
 `;
 
-export const SidebarLink = styled.div`
-    
+export const SidebarLink = styled(Link)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    text-decoration: none;
+    list-style: none;
+    transition: 0.2s ease-in-out;
+    color: #000;
+    cursor: pointer;
+
+    :hover{
+        color: #e31837;
+        transition: 0.2s ease-in-out;
+    }
+
 `;
